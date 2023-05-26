7c64c799c8e5ce9211a1550b476078a1f22a861c
fedeminatta <minattafederico@gmail.com>
2023-05-26 11:36:22 -0300

    app completa

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+
+const Resultados = styled.div`
+  color: #fff;
+  font-family: 'Lato', sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+  img {
+    display: block;
+    width: 120px;
+  }
+`;
+
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Resultado = ({ resultado }) => {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    resultado;
+  return (
+    <Resultados>
+      <img src={`https://cryptocompare.com/${IMAGEURL}`} alt="imagen cripto" />
+      <div>
+        <Precio>
+          El Precio es de: <span>{PRICE}</span>{' '}
+        </Precio>
+        <Texto>
+          El Precio más alto del dia: <span>{HIGHDAY} </span>{' '}
+        </Texto>
+        <Texto>
+          El Precio más bajo del dia: <span>{LOWDAY} </span>{' '}
+        </Texto>
+        <Texto>
+          Variación últimas 24 horas: <span>{CHANGEPCT24HOUR} </span>{' '}
+        </Texto>
+        <Texto>
+          Última actualización: <span>{LASTUPDATE} </span>{' '}
+        </Texto>
+      </div>
+    </Resultados>
+  );
+};
+export default Resultado;
